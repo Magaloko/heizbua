0cddb967cebd414aebc2a6c1cf4fbed956d10fdd
Magaloko <31704115+Magaloko@users.noreply.github.com>
2026-04-17 12:24:46 +0200

    feat: register fuelTypes, compare, leads, priceIndex routers

--- apps/web/server/routers/_app.ts
+++ apps/web/server/routers/_app.ts
@@ -1,8 +1,16 @@
 import { router } from "../trpc";
 import { healthRouter } from "./health";
+import { fuelTypesRouter } from "./fuelTypes";
+import { compareRouter } from "./compare";
+import { leadsRouter } from "./leads";
+import { priceIndexRouter } from "./priceIndex";
 
 export const appRouter = router({
   health: healthRouter,
+  fuelTypes: fuelTypesRouter,
+  compare: compareRouter,
+  leads: leadsRouter,
+  priceIndex: priceIndexRouter,
 });
 
 export type AppRouter = typeof appRouter;
