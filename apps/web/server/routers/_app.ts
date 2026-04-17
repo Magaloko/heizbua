3558bca2d3d0fbb35e6d4f3ee8b66a526c8c8e7c
Magaloko <31704115+Magaloko@users.noreply.github.com>
2026-04-17 15:32:52 +0200

    feat: register dealer routers in app router

--- apps/web/server/routers/_app.ts
+++ apps/web/server/routers/_app.ts
@@ -4,6 +4,9 @@ import { fuelTypesRouter } from "./fuelTypes";
 import { compareRouter } from "./compare";
 import { leadsRouter } from "./leads";
 import { priceIndexRouter } from "./priceIndex";
+import { dealerRouter } from "./dealer";
+import { dealerListingsRouter } from "./dealerListings";
+import { dealerZonesRouter } from "./dealerZones";
 
 export const appRouter = router({
   health: healthRouter,
@@ -11,6 +14,9 @@ export const appRouter = router({
   compare: compareRouter,
   leads: leadsRouter,
   priceIndex: priceIndexRouter,
+  dealer: dealerRouter,
+  dealerListings: dealerListingsRouter,
+  dealerZones: dealerZonesRouter,
 });
 
 export type AppRouter = typeof appRouter;
